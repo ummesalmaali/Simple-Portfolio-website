ff9659bd2f0c3022a02fffcc1a3679a09522ee44
ummesalmaali <ummes3527@gmail.com>
2021-07-02 03:21:02 +0600

    Mobile-responsive done

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     $(".js--services-section").waypoint(function (direction) {
         if (direction == "down") {
             $("nav").addClass("sticky");
+            $(".main-nav").addClass("m-0");
         } else {
             $("nav").removeClass("sticky");
+            $(".main-nav").removeClass("m-0");
         }
     });
 
