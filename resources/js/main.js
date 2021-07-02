c3b60374511c350091d70f6fa4a3e9033d92ac4b
ummesalmaali <ummes3527@gmail.com>
2021-07-02 22:01:19 +0600

    browser-compatibility-checked

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,14 +3,41 @@ $(document).ready(function () {
     $(".js--services-section").waypoint(function (direction) {
         if (direction == "down") {
             $("nav").addClass("sticky");
-            $(".main-nav").addClass("m-0");
+       
         } else {
             $("nav").removeClass("sticky");
-            $(".main-nav").removeClass("m-0");
+           
         }
     });
 
 
     //MIXITUP PORTFOLIO SECTION//
     var mixer = mixitup('.container');
+
+//    smooyh scroll for ie/edge/safari
+$("a").on('click', function(event){
+
+if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offSet().top
+    }, 800, function(){
+        window.location.hash = hash;
+    });
+    
+
+}
+
+
+
 });
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
